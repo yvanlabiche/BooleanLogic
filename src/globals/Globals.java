3ee1a9016352a4e464411e53c0aefa97d2ff2d4f
yvanlabiche <yvanlabiche@10.254.251.116>
2017-08-07 10:08:42 -0400

    globals

---
+++ src/globals/Globals.java
@@ -0,0 +1,9 @@
+package globals;
+
+import java.util.ArrayList;
+import BooleanVariables.*;
+
+public class Globals {
+	public static ArrayList Global_DNFTerms = new ArrayList();
+	public static Boolean DNFTerms_Imported = new Boolean(false);
+	}
